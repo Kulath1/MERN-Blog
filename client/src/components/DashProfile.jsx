b700056db90d28e95825fa13f4b64841043689cb
Nip <117910174+Kulath1@users.noreply.github.com>
2024-05-08 10:58:29 +0530

    Complete delete user account functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from 'flowbite-react';
+import { Alert, Button, Modal, TextInput } from 'flowbite-react';
 import { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import {useSelector} from 'react-redux';
@@ -7,11 +7,12 @@ import {app} from '../firebase'
 import {getDownloadURL, ref, uploadBytesResumable} from 'firebase/storage'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import {HiOutlineExclamationCircle} from 'react-icons/hi';
 
 export default function DashProfile(){
-    const {currentUser} = useSelector(state => state.user);
+    const {currentUser, error} = useSelector(state => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -19,6 +20,8 @@ export default function DashProfile(){
     const [imageFileUploading, setImageFileUploading] = useState(false);
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
+    // a piece of state that creates the modal popup window
+    const [showModal, setShowModal] = useState(false);
     const [formData, setFormData] = useState({});
     const dispatch = useDispatch();
 
@@ -134,7 +137,24 @@ export default function DashProfile(){
         }
     }
     
-    console.log(formData);
+    const handleDeleteUser = async ()=> {
+        setShowModal(false);
+        try{
+            dispatch(deleteUserStart());
+            const res = await fetch(`/api/user/delete/${currentUser._id}`,
+                {method: 'DELETE', }
+            );
+            const data = await res.json();
+            if(!res.ok){
+                dispatch(deleteUserFailure(data.message));
+            }else{
+                dispatch(deleteUserSuccess(data));
+            }
+        }catch(error){
+            dispatch(deleteUserFailure(error.message));
+        }
+
+    }
     return (
         <div className='max-w-lg mx-auto w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -173,7 +193,7 @@ export default function DashProfile(){
                 <Button type='submit' gradientDuoTone="purpleToBlue" outline> Update </Button>
             </form>
             <div className='text-red-500 flex justify-between mt-5'>
-                <span className='cursor-pointer'>Delete Account</span>
+                <span onClick={()=> setShowModal(true)} className='cursor-pointer'>Delete Account</span>
                 <span className='cursor-pointer'>Sign Out</span>
             </div>
             {updateUserSuccess && (
@@ -186,7 +206,40 @@ export default function DashProfile(){
                     {updateUserError}
                 </Alert>
             )}
+            {/* if there's a problem with the modal window, show an error */}
+            {error && (
+                <Alert color='failure' className='mt-5'>
+                    {error}
+                </Alert>
+            )}
+            <Modal show={showModal} onClose={()=>setShowModal(false)} popup size='md'>
+                <Modal.Header/>
+                <Modal.Body>
+                    <div className='text-center'>
+                        <HiOutlineExclamationCircle
+                            className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'
+                        />
+                        <h3 className='mb-4 text-lg text-gray-500 dark:text-gray-400'> 
+                            Are you sure you want to delete this account? 
+                        </h3>
+                        <div className='flex justify-center gap-4'>
+                            <Button color='failure' onClick={handleDeleteUser}>
+                                Yes, I'm sure
+                            </Button>
+                            <Button color='gray' onClick={()=>setShowModal(false)}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
 
         </div>
     )
 }
+
+//First make the api route
+//Add an event listener to do sth with the buttons
+//To add that event, we call a function
+//Create slices or reducers
+//After that, create a request to the particular API route and do the function that we wanna do
