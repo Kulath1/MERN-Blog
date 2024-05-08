e32a497b8be723f5413918019f90ac121d6c9b47
Nip <117910174+Kulath1@users.noreply.github.com>
2024-05-08 22:24:50 +0530

    Create API route for getting a post

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { errorHandler } from "../utils/error.js";
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
+import Post from "../models/post.model.js";
+import { title } from "process";
 
 export const test = (req, res) => {
     res.json({ message: 'The API is running' })
@@ -63,6 +65,52 @@ export const deleteUser = async (req, res, next) => {
 export const signout = (req, res, next) => {
     try{
         res.clearCookie('access_token').status(200).json("User has been signed out");
+    }catch(error){
+        next(error)
+    }
+}
+
+export const getposts = async (req, res, next) => {
+    try{
+        const startIndex = parseInt(req.query.startIndex) || 0; // to know which post to start fetching
+        const limit = parseInt(req.query.limit) || 9; //limit the number of posts shown in the dashboard
+        const sortDirection = req.query.order === 'asc' ? 1 : -1; //sort the posts in the newest to oldest order
+
+        const posts = await Post.find({
+            ...(req.query.userId && {userId: req.query.userId}), //specify the userId to which the post should be sent
+            ...(req.query.category && {category: req.query.category}), //if the search query includes a category, search for it
+            ...(req.query.slug && {slug: req.query.slug}), 
+            ...(req.query.postId && {_id: req.query.postId}), 
+            //search for a post by considering the title as well as the content of the post
+            ...(req.query.searchTerm && {
+                //or allows to in between two places(that is title and content)
+                $or:[
+                    {title: {$regex: req.query.searchTerm, $options: 'i'}},
+                    {content: {$regex: req.query.searchTerm, $options: 'i'}},
+                ],
+            }), 
+        }).sort({updatedAt: sortDirection}).skip(startIndex).limit(limit);
+
+        //getting the total number of posts
+        const totalPosts = await Post.countDocuments();
+
+        //getting the total number of posts in the last month
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth()-1,
+            now.getDate()
+        );
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo }, // 'gte' means greater than
+        });
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts,
+        });
+
     }catch(error){
         next(error);
     }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js'
 import { create } from '../controllers/post.controller.js';
+import { getposts } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getposts', getposts) // no need to verify tokens as any user can search for a post
 
 export default router;
