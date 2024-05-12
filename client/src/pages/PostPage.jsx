1cc017eab044a0ba190211e6f427f3f63707bdb3
Nip <117910174+Kulath1@users.noreply.github.com>
2024-05-12 21:33:08 +0530

    Add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
     const {postSlug} = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -35,6 +37,23 @@ export default function PostPage() {
         fetchPost();
     }, [postSlug])
 
+    
+    //to fetch the recent posts
+    useEffect(() => {
+        try{
+            const fetchRecentPosts = async () => {
+                const res = await fetch('/api/post/getposts?limit=3');
+                const data = await res.json();
+                if(res.ok){
+                    setRecentPosts(data.posts);
+                }
+            }
+            fetchRecentPosts();
+        }catch(error){
+            console.log(error.message)
+        }
+    }, [])
+
     if(loading){
         return (
             <div className="flex justify-center items-center min-h-screen">
@@ -42,7 +61,9 @@ export default function PostPage() {
             </div>
         )
     }
-  return (
+
+  
+    return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
         {/* title of the post */}
         <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
@@ -80,6 +101,20 @@ export default function PostPage() {
 
         {/* comment section */}
         <CommentSection postId={post._id}/>
+
+        {/* recent articles */}
+        <div className="flex flex-col justify-center items-center mb-5">
+            <h1 className="text-xl mt-5">Recent Articles</h1>
+            {/* posts */}
+            <div className="flex flex-wrap gap-5 mt-5 justify-center">
+                {
+                    recentPosts && recentPosts.map((post) =>(
+                        <PostCard key={post._id} post={post}/>
+                    ) )
+                }
+
+            </div>
+        </div>
 
     </main>
   )
