ef44038cdbcb4517ee7450b771277c107995982c
Nip <117910174+Kulath1@users.noreply.github.com>
2024-05-13 17:04:54 +0530

    Add dashboard section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import {Sidebar} from 'flowbite-react';
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi';
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi';
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from 'react-router-dom';
@@ -48,6 +48,20 @@ export default function DashSidebar(){
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+
+                    {/* Dashboard */}
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to='/dashboard?tab=dash'>
+                                <Sidebar.Item active={tab == 'dash' || !tab}
+                                    icon={HiChartPie}
+                                    as='div'>
+                                    Dashboard
+                                </Sidebar.Item>
+                            
+                            </Link>
+                        )
+                    }
                     {/*Profile*/}
                     <Link to='/dashboard?tab=profile'>
                    <Sidebar.Item active={tab==='profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
